c303e0b79d8fba7802c39a4253c24a3d0d67812b
Valerio Santinelli <santinelli@altralogica.it>
2020-10-15 19:14:37 +0200

    Small fixes

---
+++ assets/api/gd.lua
@@ -0,0 +1,24 @@
+---@class gd
+local m = {}
+
+---@class GraphicsDevice
+local GraphicsDevice = {}
+
+---@class TextureFormat
+local TextureFormat = {
+    ['GL_RGBA8'] = 0x8058
+}
+
+---@return gd GraphicsDevice
+function m.new() end
+
+---@param gd GraphicsDevice an instance of GraphicsDevice
+function m.init(gd) end
+
+---@param w number width
+---@param h number height
+---@param use_depth boolean use depth
+---@param format TextureFormat render target texture format
+function m.create_render_target(w, h, use_depth, format) end
+
+return m
