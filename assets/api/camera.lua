35f2cd7226570d45981d387b97b59ee368554a59
Valerio Santinelli <santinelli@altralogica.it>
2020-10-27 16:02:51 +0100

    Camera Lua documentation

---
+++ assets/api/camera.lua
@@ -0,0 +1,24 @@
+---@class camera
+local m = {}
+
+---@class Camera
+local GraphicsDevice = {}
+
+---@param adapter ViewportAdapter
+---@return camera Camera
+function m.new(adapter) end
+
+---@param camera Camera an instance of Camera
+---@return number the horizontal position
+function m.x(camera) end
+
+---@param camera Camera an instance of Camera
+---@return number the vertical position
+function m.y(camera) end
+
+---@param camera Camera
+---@param x number horizontal position
+---@param y number vertical position
+function m.set_position(camera, x, y) end
+
+return m
