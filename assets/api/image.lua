7cc5c356514001edce54bd89d554f2625b8f90ff
Valerio Santinelli <santinelli@altralogica.it>
2023-04-28 18:11:52 +0200

    Added API for image loading from physfs

--- assets/api/image.lua
+++ assets/api/image.lua
@@ -6,9 +6,15 @@ image = {}
 ---@class Image
 local Image = {}
 
+---Load an image using the SDL system
 ---@param filename string the filename including the path of the image to load
 ---@return Image the image resource id (sg_image)
 function image.load(filename) end
+
+---Load an image using the fs system (constrained to the assets folder)
+---@param filename string the filename including the path of the image to load
+---@return Image the image resource id (sg_image)
+function image.load_from_assets(filename) end
 
 ---@param image Image the image id returned by load
 ---@return number, number width and height
